e83a0dd8ffbecec10e8fa51724a527f7ebb56a11
Florent Bariod <fbariod@gmail.com>
2019-04-24 19:22:52 -0400

    basic setup for midi messages and screen recording

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,70 +2,104 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+    recorder.setPrefix(ofToDataPath("recording/frame_"));
+    recorder.setFormat("bmp");
+    recorder.startThread();
+    ofSetFrameRate(30);
+    midiIn.openPort(0);
+    midiIn.addListener(this);
+}
 
+void ofApp::exit(){
+    recorder.waitForThread();
+    recorder.stopThread();
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    // show the framerate on window title
+    std::stringstream strm;
+    strm << "fps: " << ofGetFrameRate();
+    ofSetWindowTitle(strm.str());
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    
+    // capture the image if recording is started
+    // this can slow down the rendering by a lot, so be aware of the framerate
+    if (recording){
+        screenCapture.grabScreen(0, 0, ofGetWidth(), ofGetHeight());
+        recorder.addFrame(screenCapture);
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
+    switch (key) {
+        case 'r':
+            toggleRecording();
+            break;
+        default:
+            break;
+    }
+}
 
+void ofApp::newMidiMessage(ofxMidiMessage& eventArgs){
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseEntered(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseExited(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+    
 }
+
+void ofApp::toggleRecording(){
+    recording = !recording;
+}
+
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,24 +1,46 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxMidi.h"
+#include "ofxImageSequenceRecorder.h"
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp, public ofxMidiListener {
+    
+public:
+    void setup();
+    void exit();
+    void update();
+    void draw();
+    
+    // recording
+    void toggleRecording();
+    
+    // MIDI
+    void newMidiMessage(ofxMidiMessage& eventArgs);
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
 
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+    
+private:
+    // midi
+    ofxMidiIn midiIn;
+    
+    // camera
+    ofEasyCam cam;
+    
+    // recording
+    ofxImageSequenceRecorder recorder;
+    Boolean recording;
+    ofImage screenCapture;
 };
